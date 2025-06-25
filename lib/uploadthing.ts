88604c5d68a458451596fe2f2670db33861f2d3e
sagar giri <sagargiri456>
2025-06-25 22:35:31 +0530

    100MB is not allowed which was causing error therefore assigned 32 MBs

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  noteUpload: f({ pdf: { maxFileSize: "100MB" } })
+  noteUpload: f({ pdf: { maxFileSize: "32MB" } })
     .onUploadComplete(async ({ file: _file }) => {
       
       // You can log or process the file here
