f9e6db68191b6c91c1a56afc241a96d760f8ed19
sagar giri <sagargiri456>
2025-06-25 22:31:15 +0530

    increased upload size from 8MB to 100MB

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  noteUpload: f({ pdf: { maxFileSize: "8MB" } })
+  noteUpload: f({ pdf: { maxFileSize: "100MB" } })
     .onUploadComplete(async ({ file: _file }) => {
       
       // You can log or process the file here
